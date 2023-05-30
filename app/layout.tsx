357ab18b4839a4e8a5853ec77485f57a1b2eda1a
yudihendrawan <yudihendrawan04@gmail.com>
2023-05-30 23:42:29 +0700

    add: adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head >
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4767602423321391"
+          crossOrigin="anonymous"></script>
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body >
